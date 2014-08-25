b78f961e4b51ffaa1dd879944a43bd291b8b6b67
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-08-25 16:49:58 +0200

    Config code

--- lib/sandthorn_sequel_projection.rb
+++ lib/sandthorn_sequel_projection.rb
@@ -2,5 +2,18 @@ require "sandthorn_sequel_projection/version"
 require "sandthorn_sequel_projection/projection"
 
 module SandthornSequelProjection
-  # Your code goes here...
+
+  class << self
+
+    attr_accessor :configuration
+
+    def configure
+      @configuration ||= Configuration.new
+      yield(configuration) if block_given?
+    end
+
+  end
+
+  class Configuration
+  end
 end
