512562c5a47948c34ffab89f404c02b9b543d7e4
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-08-28 11:46:51 +0200

    Add travis.yml and set up coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
 require 'sandthorn_sequel_projection'
 
 RSpec.configure do |config|
