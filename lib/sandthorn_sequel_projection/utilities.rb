95e2705bc5cbb5748b5308fc6df3cc38e47f2013
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-09-01 13:25:27 +0200

    Give up and copy Array.wrap from ActiveSupport.

--- lib/sandthorn_sequel_projection/utilities.rb
+++ lib/sandthorn_sequel_projection/utilities.rb
@@ -1,4 +1,5 @@
-require "sandthorn_sequel_projection/utilities/null_proc"
-
 module Utilities
 end
+
+require "sandthorn_sequel_projection/utilities/null_proc"
+require "sandthorn_sequel_projection/utilities/core_extensions/array_wrap"
---
+++ lib/sandthorn_sequel_projection/utilities/core_extensions/array_wrap.rb
@@ -0,0 +1,13 @@
+# Copied from ActiveSupport
+# https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/array/wrap.rb
+class Array
+  def self.wrap(object)
+    if object.nil?
+      []
+    elsif object.respond_to?(:to_ary)
+      object.to_ary || [object]
+    else
+      [object]
+    end
+  end
+end
