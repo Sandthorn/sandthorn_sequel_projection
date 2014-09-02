fa4888635601e4f1a10600b5589fd34a4c2014c8
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-09-02 16:07:22 +0200

    Track some pesky bugs by adding checks to MockEventStore

--- spec/support/mock_event_store.rb
+++ spec/support/mock_event_store.rb
@@ -5,8 +5,8 @@ module SandthornSequelProjection
       @events = Array.wrap(events)
     end
 
-    def reset
-      @events = []
+    def reset(events = [])
+      @events = events
     end
 
     def add_event(event)
@@ -16,9 +16,16 @@ module SandthornSequelProjection
     alias_method :add, :add_event
 
     def get_events(after_sequence_number: 0, take: 1)
+      unless numeric?(after_sequence_number, take)
+        raise ArgumentError, "arguments have to be numbers, received: #{after_sequence_number} and #{take}"
+      end
       start = after_sequence_number
       stop = after_sequence_number + take - 1
       Array.wrap(@events[start..stop])
+    end
+
+    def numeric?(*args)
+      args.all? { |arg| arg.is_a?(Numeric) }
     end
 
   end
